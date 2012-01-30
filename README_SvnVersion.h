d326f2d15c5f19d8546bf7567cda11f429e0eb6f
Fred Sonnenrein <fred.sonnenrein@gmx.de>
2012-01-30 08:35:01 +0000

    README_SvnVersion.h hinzugefügt
    git-svn-id: svn://svn.code.sf.net/p/itelex/misc-code/trunk@50 08b3b9a4-00d7-49ad-bbbd-872362c6a4d0

---
+++ README_SvnVersion.h
@@ -0,0 +1,8 @@
+// Die Datei SvnVersion.h wird durch SVN beim Aktualisieren aus dem Projektarchiv
+// generiert, indem die Batch-Datei SvnAfterUpdate.bat aufgerufen wird. Dies muss
+// über SVN -> Einstellungen -> Aktionsskripte -> Hinzufügen geschehen:
+//		Aktionstyp: Nach Aktualisierung
+//		Pfad der Arbeitskopie: Pfad dieser Datei hier
+//		Befehlszeile: dieser Pfad + SvnAfterUpdate.bat
+// ERSATZWEISE kann diese Datei in SvnVersion.h umbenannt werden.
+#define SVNVERSION "???" 
