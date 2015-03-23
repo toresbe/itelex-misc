7a021dfd41f44af1d77ae815efdf9e041df2bc2d
Fred Sonnenrein <fred.sonnenrein@gmx.de>
2015-03-23 21:07:29 +0000

    Alles kompiliert und Verzeichnis AlleBins angelegt
    git-svn-id: svn://svn.code.sf.net/p/itelex/misc-code/trunk@151 08b3b9a4-00d7-49ad-bbbd-872362c6a4d0

---
+++ AlleBins/MakeBins.bat
@@ -0,0 +1,5 @@
+C:\WinAVR-20100110\avr\bin\objcopy.exe -I ihex -O binary ..\AnalogModem2\default\AnalogModem2.hex .\AnalogModem.bin
+C:\WinAVR-20100110\avr\bin\objcopy.exe -I ihex -O binary ..\ED1000\default\ED1000.hex .\ED1000.bin
+C:\WinAVR-20100110\avr\bin\objcopy.exe -I ihex -O binary ..\FernschrTW39\default\FernschrTW39.hex .\FernschrTW39.bin
+C:\WinAVR-20100110\avr\bin\objcopy.exe -I ihex -O binary ..\Messgeraet\default\Messgeraet.hex .\Messgeraet.bin
+C:\WinAVR-20100110\avr\bin\objcopy.exe -I ihex -O binary ..\SeriellUndSpeicher\default\SeriellUndSpeicher.hex .\SeriellUndSpeicher.bin
