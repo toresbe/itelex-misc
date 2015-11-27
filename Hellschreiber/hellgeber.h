6e60be27c5cff74b8927dbff76de8ecadcf511df
Fred Sonnenrein <fred.sonnenrein@gmx.de>
2015-11-27 09:41:40 +0000

    Hellschreiber begonnen. Wegen konkurrierender Nutzung von Timern auf Eis.
    git-svn-id: svn://svn.code.sf.net/p/itelex/misc-code/trunk@161 08b3b9a4-00d7-49ad-bbbd-872362c6a4d0

---
+++ Hellschreiber/hellgeber.h
@@ -0,0 +1,21 @@
+// Hellgeber.h
+
+#ifndef __HELLGEBER_H__
+
+#define __HELLGEBER_H__
+
+
+#include <inttypes.h>
+#include <stdbool.h>
+
+
+extern void HellInit();
+
+extern void HellAusg(char c);
+
+extern void HellPoll();
+
+extern bool HellPuffervoll();
+
+
+#endif //def __HELLGEBER_H__
