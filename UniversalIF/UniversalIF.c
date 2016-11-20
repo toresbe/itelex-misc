ea3b244e65e93fcd020e0bb95620b6ae8f8a2185
Fred Sonnenrein <fred.sonnenrein@gmx.de>
2016-11-20 18:31:17 +0000

    UniversalInterface Project started
    git-svn-id: svn://svn.code.sf.net/p/itelex/misc-code/trunk@229 08b3b9a4-00d7-49ad-bbbd-872362c6a4d0

--- UniversalIF/UniversalIF.c
+++ UniversalIF/UniversalIF.c
@@ -11,27 +11,31 @@
 // from client	| Send to TWI Bus 	| Comment
 // -------------+-------------------+--------------------------------------------------------------
 // 0x00			|		no			| ignored to allow idle communication on client side
-// 0x01 - 0x7C  | 	own address		| Connect: received address is stored as destination address
+// 0x01 - 0x6F  | 	own address		| Connect: received address is stored as destination address
+//				|					|	status of destination is checked, confirmation is sent by
+//				|					|	0x77 or 0x78
 // 0x7D			|   status query	| query status of any TxP2 module. address for query is next byte
 //				|					|	on client side
 // 0x7E			|		no			| set internal parameter
 // 0x7F			|		no			| query internal parameter
-// 0x80 - 0x9F  | as 0x93 / 0x9E	| low 5 bits are used as baudot code to be send. MSB is sent first.
+// 0x80 - 0x9F  | as 0x93 / 0x9E	| low 5 bits are used as baudot code to be send. MSB = bit 4 
+//				|					|	is sent first.
 // 0xA0 - 0xFF  |	  	yes			| control codes. 0xAC also clears destination address
 //
 // Transmission TxP2 (TWI) -> client
-// from TWI bus	| Send to client 	| Comment
+// to client	| Orig. on TWI bus 	| Comment
 // -------------+-------------------+--------------------------------------------------------------
 // 0x00			|		no			| not allowed. On clinent side 0x00 may be used to signal idle 
-// 0x01 - 0x7C  | 		yes			| Connect: received number is also stored as destination address
-// 0x7D - 0x7F	|		no			| not allowed / ignored. on client side 0x7D is 'header' for 
-//				|					|	result of module status query. 0x7F is 'header' for result
-// 				|					| 	of internal parameter query.
-// 0x93 / 0x9E  |       no			| received baudot code is transmitted to client as 0x80 - 0x9f.  
+// 0x01 - 0x6F  | 		yes			| Connect: received number is also stored as destination address
+// 0x77			|		no			| positive confirmation
+// 0x78			|		no			| negative confirmation
+// 0x7D 		|		no			| 'header' for result of module status query. 
+// 0x7F 		|		no			| 'header' for result of internal parameter query.
+// 0x80 - 0x9F  | 0x93 / 0x9E		| received baudot code 
 //				|					|	First received bit is bit 4 on client side.
 // 0x80 - 0x9F  |	  	no			| other codes in this range are ignored
 // 0xA0 - 0xFF  | yes, except 0xA9	| control codes. 0xAC also clears destination address.
-//				|					|	0xA9 is heartbeat and is ignored 
+//				|					|	0xA9 on TWI bus is heartbeat and is not forwarded to client
 
 
 #include <avr/io.h>
