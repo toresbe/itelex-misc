2207b4ba5339ac8b1692f7d6e476f3651d83067d
Fred Sonnenrein <fred.sonnenrein@gmx.de>
2017-01-02 07:19:59 +0000

    Umbenennungen in AlleBins
    git-svn-id: svn://svn.code.sf.net/p/itelex/misc-code/trunk@268 08b3b9a4-00d7-49ad-bbbd-872362c6a4d0

--- AlleBins/MakeBins.bat
+++ AlleBins/MakeBins.bat
@@ -1,19 +1,19 @@
-C:\WinAVR-20100110\avr\bin\objcopy.exe -I ihex -O binary ..\AnalogModem2\default\AnalogModem2.hex .\TxP2_LeitungAnalog2.bin
-C:\WinAVR-20100110\avr\bin\objcopy.exe -I ihex -O binary ..\ED1000\default\ED1000.hex .\TxP2_ED1000.bin
-C:\WinAVR-20100110\avr\bin\objcopy.exe -I ihex -O binary ..\ED1000\V.21\ED1000.hex .\TxP2_V21.bin
-C:\WinAVR-20100110\avr\bin\objcopy.exe -I ihex -O binary ..\ED1000\FuerPrototyp\ED1000.hex .\TxP2_ED1000-PROTO.bin
-C:\WinAVR-20100110\avr\bin\objcopy.exe -I ihex -O binary ..\FernschrTW39\default\FernschrTW39.hex .\TxP2_TW39.bin
-C:\WinAVR-20100110\avr\bin\objcopy.exe -I ihex -O binary ..\FernschrTW39\AltePlatine\FernschrTW39.hex .\TxP2_TW39-PL10.bin
-C:\WinAVR-20100110\avr\bin\objcopy.exe -I ihex -O binary ..\FernschrTW39\USTTY-45\FernschrTW39.hex .\TxP2_TW39-US45.bin
-C:\WinAVR-20100110\avr\bin\objcopy.exe -I ihex -O binary ..\Messgeraet\default\Messgeraet.hex .\TxP2_Messgeraet.bin
-C:\WinAVR-20100110\avr\bin\objcopy.exe -I ihex -O binary ..\Messgeraet\USTTY-45\Messgeraet.hex .\TxP2_Messgeraet-US45.bin
-C:\WinAVR-20100110\avr\bin\objcopy.exe -I ihex -O binary ..\SeriellUndSpeicher\PlatVer13\SeriellUndSpeicher.hex .\TxP2_SeriellUndSpeicher.bin
-C:\WinAVR-20100110\avr\bin\objcopy.exe -I ihex -O binary ..\SeriellUndSpeicher\PlatVer21\SeriellUndSpeicher.hex .\TxP2_SeriellUndSpeicher2.bin
-C:\WinAVR-20100110\avr\bin\objcopy.exe -I ihex -O binary ..\SeriellUndSpeicher\USTTY-45_21\SeriellUndSpeicher.hex .\TxP2_SeriellUndSpeicher2-US45.bin
+C:\WinAVR-20100110\avr\bin\objcopy.exe -I ihex -O binary ..\AnalogModem2\default\AnalogModem2.hex .\itlx_LeitungAnalog2.bin
+C:\WinAVR-20100110\avr\bin\objcopy.exe -I ihex -O binary ..\ED1000\default\ED1000.hex .\itlx_ED1000.bin
+C:\WinAVR-20100110\avr\bin\objcopy.exe -I ihex -O binary ..\ED1000\V.21\ED1000.hex .\itlx_V21.bin
+C:\WinAVR-20100110\avr\bin\objcopy.exe -I ihex -O binary ..\ED1000\FuerPrototyp\ED1000.hex .\itlx_ED1000-PROTO.bin
+C:\WinAVR-20100110\avr\bin\objcopy.exe -I ihex -O binary ..\FernschrTW39\default\FernschrTW39.hex .\itlx_TW39.bin
+C:\WinAVR-20100110\avr\bin\objcopy.exe -I ihex -O binary ..\FernschrTW39\AltePlatine\FernschrTW39.hex .\itlx_TW39-PL10.bin
+C:\WinAVR-20100110\avr\bin\objcopy.exe -I ihex -O binary ..\FernschrTW39\USTTY-45\FernschrTW39.hex .\itlx_TW39-US45.bin
+C:\WinAVR-20100110\avr\bin\objcopy.exe -I ihex -O binary ..\Messgeraet\default\Messgeraet.hex .\itlx_Messgeraet.bin
+C:\WinAVR-20100110\avr\bin\objcopy.exe -I ihex -O binary ..\Messgeraet\USTTY-45\Messgeraet.hex .\itlx_Messgeraet-US45.bin
+C:\WinAVR-20100110\avr\bin\objcopy.exe -I ihex -O binary ..\SeriellUndSpeicher\PlatVer13\SeriellUndSpeicher.hex .\itlx_SeriellUndSpeicher.bin
+C:\WinAVR-20100110\avr\bin\objcopy.exe -I ihex -O binary ..\SeriellUndSpeicher\PlatVer21\SeriellUndSpeicher.hex .\itlx_SeriellUndSpeicher2.bin
+C:\WinAVR-20100110\avr\bin\objcopy.exe -I ihex -O binary ..\SeriellUndSpeicher\USTTY-45_21\SeriellUndSpeicher.hex .\itlx_SeriellUndSpeicher2-US45.bin
 rem Fuer alte Versionen des i-Telex mit Nachschlage-Tabelle:
-copy .\TxP2_LeitungAnalog2.bin AnalogModem.bin
-copy .\TxP2_ED1000.bin ED1000.bin
-copy .\TxP2_TW39.bin FernschrTW39.bin
-copy .\TxP2_Messgeraet.bin Messgeraet.bin
-copy .\TxP2_SeriellUndSpeicher.bin SeriellUndSpeicher.bin
+copy .\itlx_LeitungAnalog2.bin AnalogModem.bin
+copy .\itlx_ED1000.bin ED1000.bin
+copy .\itlx_TW39.bin FernschrTW39.bin
+copy .\itlx_Messgeraet.bin Messgeraet.bin
+copy .\itlx_SeriellUndSpeicher.bin SeriellUndSpeicher.bin
 pause
