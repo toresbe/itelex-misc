d49792f698f41a361afd027ae17086deab09e10a
Fred Sonnenrein <fred.sonnenrein@gmx.de>
2019-05-08 12:57:54 +0000

    Dritte Ebene für SeriellUndSpeicher im Test
    git-svn-id: svn://svn.code.sf.net/p/itelex/misc-code/trunk@359 08b3b9a4-00d7-49ad-bbbd-872362c6a4d0

--- MakeBins.bat
+++ MakeBins.bat
@@ -21,6 +21,8 @@ call MakeBin1.bat itlx_Messgeraet-plED Messgeraet\AufED1000\Messgeraet.hex Mega1
 call MakeBin1.bat itlx_SeriellUndSpeicher SeriellUndSpeicher\PlatVer13\SeriellUndSpeicher.hex Mega168_Quarz
 call MakeBin1.bat itlx_SeriellUndSpeicher2 SeriellUndSpeicher\PlatVer21\SeriellUndSpeicher.hex Mega168_Quarz
 rem OhneSpeicher wird nicht vertrieben
+call MakeBin1.bat itlx_SeriellUndSpeicher2-KOI7N2 SeriellUndSpeicher\KOI7N2_21\SeriellUndSpeicher.hex Mega168_Quarz
+call MakeBin1.bat itlx_SeriellUndSpeicher2-KOI8R SeriellUndSpeicher\KOI8R_21\SeriellUndSpeicher.hex Mega168_Quarz
 call MakeBin1.bat itlx_SeriellUndSpeicher2-US45 SeriellUndSpeicher\USTTY-45_21\SeriellUndSpeicher.hex Mega168_Quarz
 call MakeBin1.bat itlx_SeriellUndSpeicher2-110-7 SeriellUndSpeicher\110-7_21\SeriellUndSpeicher.hex Mega168_Quarz
 call MakeBin1.bat itlx_SeriellUndSpeicher2-110-7_EN SeriellUndSpeicher\110-7_EN_21\SeriellUndSpeicher.hex Mega168_Quarz
--- SeriellUndSpeicher/makeall.bat
+++ SeriellUndSpeicher/makeall.bat
@@ -4,3 +4,5 @@ make -C OhneSpeicher13
 make -C USTTY-45_21
 make -C 110-7_21
 make -C 110-7_EN_21
+make -C KOI8R_21
+make -C KOI7N2_21
