9e1858faa7d450769d82d4c5c76ab8fcbf5ad694
Fred Sonnenrein <fred.sonnenrein@gmx.de>
2020-09-15 10:04:08 +0000

    100 Baud version for ED1000 prepared
    git-svn-id: svn://svn.code.sf.net/p/itelex/misc-code/trunk@428 08b3b9a4-00d7-49ad-bbbd-872362c6a4d0

--- ED1000/makeall.bat
+++ ED1000/makeall.bat
@@ -1,3 +1,4 @@
 make -C default
+make -C 100Baud
 make -C FuerPrototyp
 make -C V.21
--- MakeBins.bat
+++ MakeBins.bat
@@ -2,6 +2,7 @@ mkdir AlleBins
 echo Inhalt:^<br^> >AlleBins\index.html
 call MakeBin1.bat itlx_LeitungAnalog2 AnalogModem2\default\AnalogModem2.hex Mega168_ExtTakt
 call MakeBin1.bat itlx_ED1000 ED1000\default\ED1000.hex Mega168_Quarz
+call MakeBin1.bat itlx_ED1000-100Bd ED1000\100Baud\ED1000.hex Mega168_Quarz
 call MakeBin1.bat itlx_ED1000-PROTO ED1000\FuerPrototyp\ED1000.hex Mega168_Quarz
 call MakeBin1.bat itlx_V21 ED1000\V.21\ED1000.hex Mega168_Quarz
 call MakeBin1.bat itlx_OhneFSG FernschrOhneFSG\default\FernschrOhneFSG.hex Mega168_Quarz
@@ -17,7 +18,7 @@ call MakeBin1.bat itlx_TW39plus FernschrTW39Plus\default\FernschrTW39plus.hex Me
 call MakeBin1.bat itlx_TW39plus-PL10 FernschrTW39Plus\AltePlatine\FernschrTW39plus.hex Mega168_Quarz
 call MakeBin1.bat itlx_TW39plus-DOPAUS FernschrTW39Plus\DoppelAusgabe\FernschrTW39plus.hex Mega168_Quarz
 call MakeBin1.bat itlx_TW39plus-US45 FernschrTW39Plus\USTTY-45\FernschrTW39plus.hex Mega168_Quarz
-call MakeBin1.bat itlx_TW39plus-75Baud FernschrTW39Plus\75Baud\FernschrTW39plus.hex Mega168_Quarz
+call MakeBin1.bat itlx_TW39plus-75Bd FernschrTW39Plus\75Baud\FernschrTW39plus.hex Mega168_Quarz
 call MakeBin1.bat itlx_Messgeraet Messgeraet\default\Messgeraet.hex Mega168_Quarz
 call MakeBin1.bat itlx_Messgeraet-75BD Messgeraet\75baud\Messgeraet.hex Mega168_Quarz
 call MakeBin1.bat itlx_Messgeraet-US45 Messgeraet\USTTY-45\Messgeraet.hex Mega168_Quarz
