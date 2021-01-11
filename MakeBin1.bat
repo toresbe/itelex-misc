2dd24f20ea3e43519e2b897f0cf830030feae879
Fred Sonnenrein <fred.sonnenrein@gmx.de>
2021-01-11 19:38:30 +0000

    Korrekturen der Konfigurationen
    git-svn-id: svn://svn.code.sf.net/p/itelex/misc-code/branches/2010_Studio7@467 08b3b9a4-00d7-49ad-bbbd-872362c6a4d0

--- MakeBin1.bat
+++ MakeBin1.bat
@@ -1,4 +1,4 @@
 rem Parameter: %1=Ziel %2=Quelle %3=Fuse-Musterdatei
-avr-objcopy.exe -I ihex -O binary %2 AlleBins\%1.bin
+"c:\Program Files (x86)\Atmel\Studio\7.0\toolchain\avr8\avr8-gnu-toolchain\bin\avr-objcopy.exe" -I ihex -O binary %2 AlleBins\%1.bin
 copy FuseMuster\%3.txt AlleBins\%1.txt
 echo %1 ^<br^> >>AlleBins\index.html
